79706e8b524d89cd5ac25375d593106d4f4998aa
Alaaa1 <alaa0602@hotmail.com>
2023-07-19 00:25:11 +0300

    new branch frontendAuthentication

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "react-cookie";
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
